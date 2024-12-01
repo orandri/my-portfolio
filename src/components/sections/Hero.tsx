fe9c3688c16cafeb40a6767d5879863b6bf46128
Olivier <olivier91randria@gmail.com>
2024-12-01 18:43:34 +0100

    Retire RS

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -31,7 +31,7 @@ const Hero = () => {
       </div>
       <ComputersCanvas />
       
-   {/* Conteneur pour aligner les images sur la même ligne */}
+   {/* Conteneur pour aligner les images sur la même ligne 
    <div className="flex items-center gap-4 mt-4">
         <div
             onClick={() => {
@@ -75,7 +75,7 @@ const Hero = () => {
               <path d="M4.98 3.5c0 1.381-1.11 2.5-2.48 2.5s-2.48-1.119-2.48-2.5c0-1.38 1.11-2.5 2.48-2.5s2.48 1.12 2.48 2.5zm.02 4.5h-5v16h5v-16zm7.982 0h-4.968v16h4.969v-8.399c0-4.67 6.029-5.052 6.029 0v8.399h4.988v-10.131c0-7.88-8.922-7.593-11.018-3.714v-2.155z" />
             </svg>
           </div>
-      </div>
+      </div>*/}
 
 
       <div className="xs:bottom-10 absolute bottom-32 flex w-full items-center justify-center">
